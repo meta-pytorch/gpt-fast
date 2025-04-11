54d610cd157e80e1210dff0e316305819840e972
Manuel Candales <mcandales@fb.com>
2025-04-11 15:36:19 -0400

    disable compile for rms_norm

--- model.py
+++ model.py
@@ -233,20 +233,20 @@ class FeedForward(nn.Module):
 class RMSNorm(nn.Module):
     def __init__(self, dim: int, eps: float = 1e-5):
         super().__init__()
-        # self.normalized_shape = (dim,)
+        self.normalized_shape = (dim,)
         self.eps = eps
         self.weight = nn.Parameter(torch.ones(dim))
 
-    def _norm(self, x):
-        return x * torch.rsqrt(torch.mean(x * x, dim=-1, keepdim=True) + self.eps)
-
-    def forward(self, x: Tensor) -> Tensor:
-        output = self._norm(x.float()).type_as(x)
-        return output * self.weight
-
-    # @torch.compiler.disable
+    # def _norm(self, x):
+    #     return x * torch.rsqrt(torch.mean(x * x, dim=-1, keepdim=True) + self.eps)
+    #
     # def forward(self, x: Tensor) -> Tensor:
-    #     return F.rms_norm(x, self.normalized_shape, self.weight, self.eps)
+    #     output = self._norm(x.float()).type_as(x)
+    #     return output * self.weight
+
+    @torch.compiler.disable
+    def forward(self, x: Tensor) -> Tensor:
+        return F.rms_norm(x, self.normalized_shape, self.weight, self.eps)
 
 
 def apply_rope_scaling(freqs: torch.Tensor, rope_scaling: Optional[dict] = None):
